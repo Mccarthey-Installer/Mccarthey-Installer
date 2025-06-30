8fb4ebfc349947a66e1d8dffbd13407ae152f952
Mccarthey-Installer <miprincesabukele@gmail.com>
2025-06-30 11:56:40 -0600

    Update scrip.sh

--- main/scrip.sh
+++ main/scrip.sh
@@ -532,7 +532,7 @@ function verificar_online() {
                 fi
             fi
             # NUEVO ORDEN DE COLUMNAS EN PRINTF
-            printf "${AMARILLO}%-15s ${COLOR_ESTADO}%-15s ${AMARILLO}%-12s ${AZUL}%-25s${NC}\n" "$USUARIO" "$ESTADO" "$MOVILES_NUM" "$DETALLES"
+            printf "${AMARILLO}%-15s ${COLOR_ESTADO}%-15s ${AMARILLO}%-13s ${AZUL}%-25s${NC}\n" "$USUARIO" "$ESTADO" "$MOVILES_NUM" "$DETALLES"
         fi
     done < "$REGISTROS"
 
