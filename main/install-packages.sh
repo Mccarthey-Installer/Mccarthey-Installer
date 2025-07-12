3b99c45c5798f54c4cdae0aaa9c9efd88bfbadc6
Mccarthey-Installer <miprincesabukele@gmail.com>
2025-07-12 16:15:42 -0600

    Update install-packages.sh

--- main/install-packages.sh
+++ main/install-packages.sh
@@ -136,10 +136,9 @@ Description=Badvpn UDPGW Service for VPN Tunneling on Port 7300
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 \\
-  --max-clients 2048 \\
-  --max-connections-for-client 64 \\
-  --udpgw-connection-buffer-size 1024
+ExecStart=/usr/bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 \
+  --max-clients 2048 \
+  --max-connections-for-client 64
 Type=simple
 Restart=always
 RestartSec=5
@@ -165,6 +164,7 @@ StandardError=journal
 [Install]
 WantedBy=multi-user.target
 Alias=badvpn.service
+
 EOF
 
 systemctl daemon-reload
