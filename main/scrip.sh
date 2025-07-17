789ad5545abda077ad922431c8ab59c664328f1a
Mccarthey-Installer <miprincesabukele@gmail.com>
2025-07-17 14:15:58 -0600

    Update scrip.sh

--- main/scrip.sh
+++ main/scrip.sh
@@ -693,7 +693,7 @@ function verificar_online() {
                         HORA=$(echo "$LOGOUT_LINE" | awk '{print $3}')
                         MES_ES=${month_map["$MES"]}
                         if [ -z "$MES_ES" ]; then MES_ES="$MES"; fi
-                        HORA_SIMPLE=$(date -d "$MES $DIA $HORA" +"%I:%M %p" 2>/dev/null)
+                        HORA_SIMPLE=$(TZ="America/El_Salvador" date -d "$MES $DIA $HORA UTC" +"%I:%M %p" 2>/dev/null)
                         if [[ -z "$HORA_SIMPLE" ]]; then HORA_SIMPLE="$HORA"; fi
                         DETALLES="📅 Última: $DIA de $MES_ES $HORA_SIMPLE"
                     else
@@ -704,7 +704,7 @@ function verificar_online() {
                             HORA=$(echo "$LOGIN_LINE" | awk '{print $3}')
                             MES_ES=${month_map["$MES"]}
                             if [ -z "$MES_ES" ]; then MES_ES="$MES"; fi
-                            HORA_SIMPLE=$(date -d "$MES $DIA $HORA" +"%I:%M %p" 2>/dev/null)
+                            HORA_SIMPLE=$(TZ="America/El_Salvador" date -d "$MES $DIA $HORA UTC" +"%I:%M %p" 2>/dev/null)
                             if [[ -z "$HORA_SIMPLE" ]]; then HORA_SIMPLE="$HORA"; fi
                             DETALLES="📅 Última: $DIA de $MES_ES $HORA_SIMPLE"
                         fi
@@ -721,6 +721,7 @@ function verificar_online() {
     echo -e "${CIAN}================================================${NC}"
     read -p "$(echo -e ${AZUL}Presiona Enter para continuar...${NC})"
 }
+
 
 
 
