aa5bb65ae182c57e2ba70a0226b60747dc3a83fd
Mccarthey-Installer <miprincesabukele@gmail.com>
2025-10-28 18:33:29 -0600

    Update panel header emoji in scrip.sh

--- main/scrip.sh
+++ main/scrip.sh
@@ -3109,7 +3109,7 @@ while true; do
     clear
     barra_sistema
     echo
-    echo -e "${VIOLETA}======💵💫PANEL DE USUARIOS VPN/SSH ======${NC}"
+    echo -e "${VIOLETA}======💫🐳PANEL DE USUARIOS VPN/SSH ======${NC}"
     echo -e "${AMARILLO_SUAVE}1. 🆕 Crear usuario${NC}"
     echo -e "${AMARILLO_SUAVE}2. 📋 Ver registros${NC}"
     echo -e "${AMARILLO_SUAVE}3. 🗑️ Eliminar usuario${NC}"
