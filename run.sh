8a9da59c1aed5c8f6c01ba65d20367d35ddbda15
Mccarthey-Installer <miprincesabukele@gmail.com>
2025-12-14 11:23:41 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ ENCRYPTED_FILE="payload.enc"
 read -s -p "Contraseña: " PASS
 echo
 
-# Pasar la contraseña por STDIN (forma segura)
-printf "%s" "$PASS" | openssl enc -d -aes-256-cbc -pbkdf2 -iter 10000 \
+openssl enc -d -aes-256-cbc -pbkdf2 -iter 10000 \
   -in "$ENCRYPTED_FILE" \
-  -pass stdin | bash
+  -pass pass:"$PASS" | bash
