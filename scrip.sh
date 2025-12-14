cb755827f71fdc905e7022909a585ba17b147fc2
Mccarthey-Installer <miprincesabukele@gmail.com>
2025-12-14 00:04:05 -0600

    Update scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -6,6 +6,6 @@ ENCRYPTED_FILE="scrip.enc"
 read -s -p "Contraseña: " PASS
 echo
 
-openssl enc -d -aes-256-cbc -pbkdf2 -iter 10000 \
+openssl enc -d -aes-256-cbc -salt -pbkdf2 -iter 10000 \
   -in "$ENCRYPTED_FILE" \
   -pass pass:"$PASS" | bash
